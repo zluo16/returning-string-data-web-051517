33ed4c738bc1ca1f2559bb84a56cf1f25ee0e039
zluo16 <zluo@alumni.berklee.edu>
2017-07-08 13:52:09 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,11 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def body
+    post = Post.find(params[:id])
+    render plain: post.description
+  end
+
 private
   # Use callbacks to share common setup or constraints between actions.
   def set_post
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
+
+  get 'posts/:id/body' => 'posts#body'
+
 end
